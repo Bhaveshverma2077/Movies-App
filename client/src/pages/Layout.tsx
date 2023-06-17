44b63ee46441a4bd9742f081e7ac632b585adfd5
Bhavesh Verma <bhaveshverma2077@gmail.com>
2023-06-17 19:33:51 +0530

    fix bug: scroll position does not change when changing page

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,7 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import TopAppBar from "../components/TopAppBar";
+import { useEffect } from "react";
 
 const Layout: React.FC = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <>
       <TopAppBar />
