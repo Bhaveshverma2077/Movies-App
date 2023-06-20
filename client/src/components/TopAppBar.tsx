534d9c86d9a13429d84f250e83c357b0d13e86ac
Bhavesh Verma <bhaveshverma2077@gmail.com>
2023-06-20 18:48:39 +0530

    make TopAppBar transparent and hide appBar on scroll down and show when scroll up

--- client/src/components/TopAppBar.tsx
+++ client/src/components/TopAppBar.tsx
@@ -8,6 +8,8 @@ import {
   Link,
   MenuItem,
   Tooltip,
+  Slide,
+  useScrollTrigger,
 } from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import MenuIcon from "@mui/icons-material/Menu";
@@ -45,74 +47,78 @@ const TopAppBar = () => {
     setAnchorElUser(null);
   };
 
+  const trigger = useScrollTrigger();
+
   return (
-    <AppBar color="primary" position="sticky">
-      <Toolbar className="flex justify-between">
-        <Box className="sm:hidden block">
-          <IconButton onClick={handleOpenNavMenu}>
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            open={!!anchorElNav}
-            anchorEl={anchorElNav}
-            onClose={handleCloseNavMenu}
-            anchorOrigin={{ horizontal: "left", vertical: "bottom" }}
-            transformOrigin={{ horizontal: "left", vertical: "top" }}
-          >
-            {pages.map((page) => (
-              <MenuItem
-                key={page.title}
-                onClick={() => {
-                  navigate(page.path);
-                }}
-              >
-                {page.title}
-              </MenuItem>
-            ))}
-          </Menu>
-        </Box>
-        <Box className="flex">
-          <Typography variant="h6" fontWeight="700">
-            <Link component={LinkReactRouter} underline="none" to="/">
-              LOGO
-            </Link>
-          </Typography>
-          <Box className="ml-16 sm:flex hidden sm:items-center sm:gap-6">
-            {pages.map((page) => (
-              <Link
-                component={LinkReactRouter}
-                key={page.title}
-                className="text-white block cursor-pointer"
-                underline="none"
-                to={page.path}
-              >
-                {page.title}
-              </Link>
-            ))}
-          </Box>
-        </Box>
-        <Box>
-          <Tooltip title="Open settings">
-            <IconButton onClick={handleOpenUserMenu}>
-              <AccountCircleIcon />
+    <Slide appear={false} direction={"down"} in={!trigger}>
+      <AppBar className="bg-transparent">
+        <Toolbar className="flex justify-between" variant="dense">
+          <Box className="sm:hidden block">
+            <IconButton onClick={handleOpenNavMenu}>
+              <MenuIcon />
             </IconButton>
-          </Tooltip>
-          <Menu
-            open={!!anchorElUser}
-            anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-            transformOrigin={{ vertical: "top", horizontal: "right" }}
-            anchorEl={anchorElUser}
-            onClose={handleCloseUserMenu}
-          >
-            {settings.map((setting) => (
-              <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                <Typography textAlign="center"> {setting}</Typography>
-              </MenuItem>
-            ))}
-          </Menu>
-        </Box>
-      </Toolbar>
-    </AppBar>
+            <Menu
+              open={!!anchorElNav}
+              anchorEl={anchorElNav}
+              onClose={handleCloseNavMenu}
+              anchorOrigin={{ horizontal: "left", vertical: "bottom" }}
+              transformOrigin={{ horizontal: "left", vertical: "top" }}
+            >
+              {pages.map((page) => (
+                <MenuItem
+                  key={page.title}
+                  onClick={() => {
+                    navigate(page.path);
+                  }}
+                >
+                  {page.title}
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box className="flex">
+            <Typography variant="h6" fontWeight="700">
+              <Link component={LinkReactRouter} underline="none" to="/">
+                LOGO
+              </Link>
+            </Typography>
+            <Box className="ml-16 sm:flex hidden sm:items-center sm:gap-6">
+              {pages.map((page) => (
+                <Link
+                  component={LinkReactRouter}
+                  key={page.title}
+                  className="text-white block cursor-pointer"
+                  underline="none"
+                  to={page.path}
+                >
+                  {page.title}
+                </Link>
+              ))}
+            </Box>
+          </Box>
+          <Box>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu}>
+                <AccountCircleIcon />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              open={!!anchorElUser}
+              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+              transformOrigin={{ vertical: "top", horizontal: "right" }}
+              anchorEl={anchorElUser}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center"> {setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Slide>
   );
 };
 
