38d0b52d9230bdea84c047e8472bd0e9bec8097e
Bhavesh Verma <bhaveshverma2077@gmail.com>
2023-06-23 14:21:28 +0530

    switching between movie and tvshow now available in genrePage

--- client/src/pages/GenrePage.tsx
+++ client/src/pages/GenrePage.tsx
@@ -1,107 +1,71 @@
 import TopAppBar from "../components/TopAppBar";
 import GenreItem from "../components/GenreItem";
-import { Grid, Container, Box } from "@mui/material";
+import { Grid, Container, Box, Chip } from "@mui/material";
 import { useSelector } from "react-redux";
 import store, { RootState } from "../store";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchGenre as fetchGenresMovies } from "../store/movies-slice";
-
-const genresList = [
-  {
-    id: 28,
-    name: "Action",
-  },
-  {
-    id: 12,
-    name: "Adventure",
-  },
-  {
-    id: 16,
-    name: "Animation",
-  },
-  {
-    id: 35,
-    name: "Comedy",
-  },
-  {
-    id: 80,
-    name: "Crime",
-  },
-  {
-    id: 99,
-    name: "Documentary",
-  },
-  {
-    id: 18,
-    name: "Drama",
-  },
-  {
-    id: 10751,
-    name: "Family",
-  },
-  {
-    id: 14,
-    name: "Fantasy",
-  },
-  {
-    id: 36,
-    name: "History",
-  },
-  {
-    id: 27,
-    name: "Horror",
-  },
-  {
-    id: 10402,
-    name: "Music",
-  },
-  {
-    id: 9648,
-    name: "Mystery",
-  },
-  {
-    id: 10749,
-    name: "Romance",
-  },
-  {
-    id: 878,
-    name: "Science Fiction",
-  },
-  {
-    id: 10770,
-    name: "TV Movie",
-  },
-  {
-    id: 53,
-    name: "Thriller",
-  },
-  {
-    id: 10752,
-    name: "War",
-  },
-  {
-    id: 37,
-    name: "Western",
-  },
-];
+import { fetchGenre as fetchGenresTvShows } from "../store/tv-shows-slice";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const GenrePage: React.FC = () => {
+  const route = useLocation().pathname;
+  const navigate = useNavigate();
+
+  const [movieOrTvShow, setMovieOrTvShow] = useState<"movie" | "tvShow">(
+    route.startsWith("/movie") ? "movie" : "tvShow"
+  );
+
   const genrePageMovies = useSelector(
     (state: RootState) => state.movies.genrePageMovies
   );
 
+  const genrePageTvShows = useSelector(
+    (state: RootState) => state.tvs.genrePageTvShows
+  );
+
   useEffect(() => {
     store.dispatch(fetchGenresMovies());
+    store.dispatch(fetchGenresTvShows());
   }, []);
+  console.log(genrePageTvShows);
+
   return (
     <>
       <Box className="py-0 sm:py-0 px-0 sm:px-6 lg:px-36">
+        <Box className="py-6">
+          <Chip
+            variant={movieOrTvShow == "movie" ? "filled" : "outlined"}
+            clickable
+            label="Movies"
+            onClick={() => {
+              navigate("/movie/genre");
+              setMovieOrTvShow("movie");
+            }}
+          ></Chip>
+          <Chip
+            variant={movieOrTvShow == "movie" ? "outlined" : "filled"}
+            clickable
+            label="Tv Shows"
+            onClick={() => {
+              navigate("/tv-show/genre");
+              setMovieOrTvShow("tvShow");
+            }}
+          ></Chip>
+        </Box>
         <Grid container spacing={{ xs: 0, sm: 3 }}>
-          {genrePageMovies.map((genre, i) => (
-            <Grid item key={i} lg={3} md={4} sm={6} xs={12}>
-              <GenreItem {...genre} />
-            </Grid>
-          ))}
+          {movieOrTvShow == "movie" &&
+            genrePageMovies.map((genreIem) => (
+              <Grid item key={genreIem.genre.id} lg={3} md={4} sm={6} xs={12}>
+                <GenreItem {...genreIem} movieOrTvShow={movieOrTvShow} />
+              </Grid>
+            ))}
+          {movieOrTvShow == "tvShow" &&
+            genrePageTvShows.map((genreIem) => (
+              <Grid item key={genreIem.genre.id} lg={3} md={4} sm={6} xs={12}>
+                <GenreItem {...genreIem} movieOrTvShow={movieOrTvShow} />
+              </Grid>
+            ))}
         </Grid>
       </Box>
     </>
