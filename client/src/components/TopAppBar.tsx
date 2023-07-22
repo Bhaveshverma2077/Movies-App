fe0a933b288f23b3ec0e3d3b401bbf115cab3b3c
Bhavesh Verma <bhaveshverma2077@gmail.com>
2023-07-22 21:31:06 +0530

    add logout and active link style

--- client/src/components/TopAppBar.tsx
+++ client/src/components/TopAppBar.tsx
@@ -11,27 +11,78 @@ import {
   Slide,
   useScrollTrigger,
   LinearProgress,
+  Button,
 } from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import MenuIcon from "@mui/icons-material/Menu";
-import React, { useState } from "react";
-import { Link as LinkReactRouter, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import {
+  Link as LinkReactRouter,
+  NavLink,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 import { useSelector } from "react-redux";
-import { RootState } from "../store";
+import store, { RootState } from "../store";
+import { userActions } from "../store/users-slice";
 
 const pages = [
   { title: "Movies", path: "/movie" },
   { title: "Tv Series", path: "/tv-show" },
   { title: "Genres", path: "/movie/genre" },
   { title: "Search", path: "/movie/search" },
-  { title: "Downloads", path: "/" },
 ];
-const settings = ["Profile", "Account", "Logout"];
+const settings = [
+  {
+    name: "Profile",
+    handler: () => {},
+  },
+  {
+    name: "Account",
+    handler: () => {},
+  },
+  {
+    name: "Logout",
+    handler: () => {
+      store.dispatch(userActions.logoutUser());
+    },
+  },
+];
 
 const TopAppBar = () => {
+  // To set active NavLink Style
+  const pathName = useLocation().pathname;
+
+  const [activeNavLinkIndex, setActiveNavLinkIndex] = useState<
+    null | 0 | 1 | 2 | 3
+  >(null);
+
+  useEffect(() => {
+    if (pathName == "/movie" || pathName == "/movie" + "/") {
+      setActiveNavLinkIndex(0);
+    } else if (pathName == "/tv-show" || pathName == "/tv-show" + "/") {
+      setActiveNavLinkIndex(1);
+    } else if (
+      pathName.startsWith("/movie/genre") ||
+      pathName.startsWith("/tv-show/genre")
+    ) {
+      setActiveNavLinkIndex(2);
+    } else if (
+      pathName.startsWith("/movie/search") ||
+      pathName.startsWith("/tv-show/search")
+    ) {
+      setActiveNavLinkIndex(3);
+    } else {
+      setActiveNavLinkIndex(null);
+    }
+  }, [pathName]);
+  //******************************/
+
   const isLoading = useSelector(
     (state: RootState) => state.others.appBarLoading
   );
+
+  const user = useSelector((state: RootState) => state.users.userId);
 
   const navigate = useNavigate();
 
@@ -50,15 +101,18 @@ const TopAppBar = () => {
     setAnchorElNav(null);
   };
 
-  const handleCloseUserMenu = () => {
+  const handleCloseUserMenu = (handler?: () => void) => {
     setAnchorElUser(null);
+    if (handler) {
+      handler();
+    }
   };
 
   const trigger = useScrollTrigger();
 
   return (
     <Slide appear={false} direction={"down"} in={!trigger}>
-      <AppBar className="bg-transparent">
+      <AppBar className="bg-black bg-opacity-80">
         <Toolbar className="flex justify-between" variant="dense">
           <Box className="sm:hidden block">
             <IconButton onClick={handleOpenNavMenu}>
@@ -86,15 +140,17 @@ const TopAppBar = () => {
           <Box className="flex">
             <Typography variant="h6" fontWeight="700">
               <Link component={LinkReactRouter} underline="none" to="/">
-                LOGO
+                M Flix
               </Link>
             </Typography>
             <Box className="ml-16 sm:flex hidden sm:items-center sm:gap-6">
-              {pages.map((page) => (
+              {pages.map((page, i) => (
                 <Link
                   component={LinkReactRouter}
                   key={page.title}
-                  className="text-white block cursor-pointer"
+                  className={`text-white block cursor-pointer transition-transform ${
+                    activeNavLinkIndex === i ? "text-[#e03131]" : ""
+                  }`}
                   underline="none"
                   to={page.path}
                 >
@@ -104,22 +160,40 @@ const TopAppBar = () => {
             </Box>
           </Box>
           <Box>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu}>
-                <AccountCircleIcon />
-              </IconButton>
-            </Tooltip>
+            {user ? (
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu}>
+                  <AccountCircleIcon />
+                </IconButton>
+              </Tooltip>
+            ) : (
+              <Link
+                component={LinkReactRouter}
+                onClick={() => {
+                  store.dispatch(userActions.logoutUser());
+                }}
+                className="cursor-pointer block border-[1px] text-base border-solid py-0 px-2 text-[#e03131] hover:bg-[#e03131] hover:text-zinc-200 transition-colors border-[#e03131]"
+                underline="none"
+                to={"/login"}
+              >
+                Login
+              </Link>
+            )}
+
             <Menu
               open={!!anchorElUser}
               className="bg-transparent"
               anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
               transformOrigin={{ vertical: "top", horizontal: "right" }}
               anchorEl={anchorElUser}
-              onClose={handleCloseUserMenu}
+              onClose={() => handleCloseUserMenu()}
             >
               {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center"> {setting}</Typography>
+                <MenuItem
+                  key={setting.name}
+                  onClick={() => handleCloseUserMenu(setting.handler)}
+                >
+                  <Typography textAlign="center"> {setting.name}</Typography>
                 </MenuItem>
               ))}
             </Menu>
