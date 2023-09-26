1a45d9329499b4bb99d92b992751b1e4e46dd9ed
Bhavesh Verma <bhaveshverma2077@gmail.com>
2023-09-26 18:58:03 +0530

    add catch-all route

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
-import movieRouter from "./routes/movies";
+import { connect } from "mongoose";
+import moviesRouter from "./routes/movies";
+import usersRouter from "./routes/users";
 import tvShowsRouter from "./routes/tv-shows";
 import dotenv from "dotenv";
 import cors from "cors";
+import auth from "./middleware/auth";
 
 dotenv.config();
 const app = express();
@@ -10,10 +13,19 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(auth);
 
-app.use("/movies", movieRouter);
+app.use("/users", usersRouter);
+app.use("/movies", moviesRouter);
 app.use("/tv-shows", tvShowsRouter);
 
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
+app.route("/*").all((req, res, next) => {
+  res.status(404).json({ message: "requested resource not found" });
+});
+
+connect(process.env.DATABASE_URL!).then(() => {
+  app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
+  });
 });
